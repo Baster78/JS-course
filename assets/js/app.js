d2e5c16b8bf4a03dd581002500de6fbe6c91a812
Cardinal <avror1g@yandex.ru>
2023-11-13 14:55:18 +0300

    lesson 1 finish

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,12 +23,22 @@
 
 'use strict';
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let personalMovieDB = {
+const personalMovieDB = {
 	count: numberOfFilms,
-	movies: 0,
-	actors: 0,
+	movies: {},
+	actors: {},
 	genres: [],
 	privat: false
 };
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+	b = prompt('На сколько оцените его?', ''),
+	c = prompt('Один из последних просмотренных фильмов?', ''),
+	d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
