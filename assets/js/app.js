11913bbc59b795742aec4bae146f9da2400141c8
Cardinal <avror1g@yandex.ru>
2023-11-14 14:57:07 +0300

    lesson 24

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,50 @@
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
 'use strict';
 
-// let result = '';
-// const length = 7;
+// Код возьмите из предыдущего домашнего задания
 
-// for (let i = 1; i < length; i++) {
-//    for (let j = 0; j < i; j++) {
-//       result += '*';
+// const numberOfFilms = prompt('Сколько филомом вы уже посмотрели?', '');
+// const personalMovieDB = {
+//    count: numberOfFilms,
+//    movies: {},
+//    actors: {},
+//    genres: [],
+//    privat: false,
+// };
+
+// for (let i = 0; i < 2; i++) {
+//    const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', '');
+
+//    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//       personalMovieDB.movies[a] = b;
+//       console.log('done');
+//    } else {
+//       console.log('error');
+//       i--;
 //    }
-//    result += '\n';
 // }
 
-// console.log(result);
-
-first: for (let i = 0; i < 3; i++) {
-   console.log(`First level: ${i}`);
-   for (let j = 0; j < 3; j++) {
-      console.log(`Second level: ${j}`);
-      for (let k = 0; k < 3; k++) {
-         if (k === 2) continue first;
-         console.log(`Third level: ${k}`);
-      }
-   }
-}
-
-function fifthTask() {
-   const arrayOfNumbers = [];
-
-   // Пишем решение вот тут
-
-   for (let i = 5; i <= 10; i++) {
-      arrayOfNumbers[i];
-   }
-   // Не трогаем
-   return arrayOfNumbers;
-}
-
-fifthTask();
+// if (personalMovieDB.count < 10) {
+//    console.log('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//    console.log('Вы классический зритель!');
+// } else if (personalMovieDB.count >= 30) {
+//    console.log('Вы киноман! ');
+// } else {
+//    console.log('Произошла ошибка');
+// }
+// console.log(personalMovieDB);
