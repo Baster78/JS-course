a44b472e5274218d05ae3c592f93dc4339aa9385
Cardinal <avror1g@yandex.ru>
2023-11-15 14:54:56 +0300

    lesson 29

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -1,15 +1,1 @@
 'use strict';
-
-const usdCourse = 28;
-const discount = 0.9;
-
-function convert(amount, curr) {
-   return curr * amount;
-}
-
-function promotion(result) {
-   console.log(result * discount);
-}
-
-const res = convert(500, usdCourse);
-promotion(res);
