611c73fe133d774a3a07510e72ad33ce497d5649
Cardinal <avror1g@yandex.ru>
2023-11-22 13:46:39 +0300

    lesson 42

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -1,35 +1,19 @@
 'use strict';
 
-const arr = [10, 8, 3, 12, 25];
-arr.sort(compareNum);
-console.log(arr);
+const box = document.getElementById('box');
+console.log(box);
 
-function compareNum(a, b) {
-   return a - b;
-}
+const btns = document.getElementsByTagName('button');
+console.log(btns[1]);
 
-// arr.pop();
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
 
-// console.log(arr);
+const hearts = document.querySelectorAll('.heart');
+console.log(hearts);
 
-// arr.push(10);
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
 
-// console.log(arr);
-
-// for (let i = 0; i < arr.length; i++) {
-//    console.log(arr[i]);
-// }
-
-// for (const value of arr) {
-//    console.log(value);
-// }
-
-// arr.forEach(function (item, i, arr) {
-//    console.log(`${i}: ${item} Внутри массива ${arr}`);
-// });
-
-// const str = prompt('', '');
-
-// const product = str.split(', ');
-// product.sort();
-// console.log(product.join('; '));
+const oneBox = document.querySelector('.box');
+console.log(oneBox);
