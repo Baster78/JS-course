19e961dce75f8ee73fe6e4e0f686a58af496f01a
Cardinal <avror1g@yandex.ru>
2023-11-22 15:46:56 +0300

    lesson44

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -1,19 +1,48 @@
 'use strict';
+/* Задания на урок:
 
-const box = document.getElementById('box');
-console.log(box);
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
 
-const btns = document.getElementsByTagName('button');
-console.log(btns[1]);
+2) Изменить жанр фильма, поменять "комедия" на "драма"
 
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
 
-const hearts = document.querySelectorAll('.heart');
-console.log(hearts);
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+5) Добавить нумерацию выведенных фильмов */
 
-const oneBox = document.querySelector('.box');
-console.log(oneBox);
+'use strict';
+
+const movieDB = {
+   movies: [
+      'Логан',
+      'Лига справедливости',
+      'Ла-ла лэнд',
+      'Одержимость',
+      'Скотт Пилигрим против...',
+   ],
+};
+
+const adv = document.querySelectorAll('.promo__adv img'),
+   poster = document.querySelector('.promo__bg'),
+   genre = poster.querySelector('.promo__genre'),
+   movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+   item.remove();
+});
+
+genre.textContent = 'Драма';
+poster.style.backgroundImage = 'url(assets/images/bg.jpg)';
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+   movieList.innerHTML += `<li class="promo__interactive-item">${i + 1} ${film}
+      <div class="delete"></div>
+</li>`;
+});
