d1a558bdb66fc08dd8cfb3a8c40c5faee650d06e
Cardinal <avror1g@yandex.ru>
2023-12-12 16:33:51 +0300

    lesson64

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,38 @@
-const btns = document.querySelectorAll('button'),
-   wrapper = document.querySelector('.btn-block');
+window.addEventListener('DOMContentLoaded', () => {
+   const tabs = document.querySelectorAll('.tabheader__item'),
+      tabsContent = document.querySelectorAll('.tabcontent'),
+      tabsParent = document.querySelector('.tabheader__items');
 
-// console.log(btns[0].classList.length);
-// console.log(btns[0].classList.item(0));
-// console.log(btns[1].classList.add('red'));
-// console.log(btns[0].classList.remove('blue'));
-// console.log(btns[0].classList.toggle('blue'));
+   function hideTabContent() {
+      tabsContent.forEach(item => {
+         item.classList.add('hide');
+         item.classList.remove('show', 'fade');
+      });
 
-// if (btns[1].classList.contains('red')) {
-//    console.log('red');
-// }
-
-btns[0].addEventListener('click', () => {
-   // if (!btns[1].classList.contains('red')) {
-   //    btns[1].classList.add('red');
-   // } else {
-   //    btns[1].classList.remove('red');
-   // }
-   btns[1].classList.toggle('red');
-});
-
-wrapper.addEventListener('click', event => {
-   if (event.target && event.target.matches('BUTTON.red')) {
-      console.log('Hello');
+      tabs.forEach(item => {
+         item.classList.remove('tabheader__item_active');
+      });
    }
-});
 
-const btn = document.createElement('button');
-btn.classList.add('red');
-wrapper.append(btn);
+   function showTabContent(i = 0) {
+      tabsContent[i].classList.add('show', 'fade');
+      tabsContent[i].classList.remove('hide');
+      tabs[i].classList.add('tabheader__item_active');
+   }
+
+   hideTabContent();
+   showTabContent();
+
+   tabsParent.addEventListener('click', event => {
+      const target = event.target;
+
+      if (target && target.classList.contains('tabheader__item')) {
+         tabs.forEach((item, i) => {
+            if (target == item) {
+               hideTabContent();
+               showTabContent(i);
+            }
+         });
+      }
+   });
+});
