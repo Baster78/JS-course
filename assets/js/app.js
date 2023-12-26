e0a8cc6edca95eec5e48c65b8da5e000f90166a8
Cardinal <avror1g@yandex.ru>
2023-12-26 13:27:51 +0300

    lesson 80

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
-window.addEventListener('DOMContentLoaded', () => {
-   const tabs = document.querySelectorAll('.tabheader__item'),
+window.addEventListener('DOMContentLoaded', function () {
+   // Tabs
+
+   let tabs = document.querySelectorAll('.tabheader__item'),
       tabsContent = document.querySelectorAll('.tabcontent'),
       tabsParent = document.querySelector('.tabheader__items');
 
@@ -23,9 +25,8 @@ window.addEventListener('DOMContentLoaded', () => {
    hideTabContent();
    showTabContent();
 
-   tabsParent.addEventListener('click', event => {
+   tabsParent.addEventListener('click', function (event) {
       const target = event.target;
-
       if (target && target.classList.contains('tabheader__item')) {
          tabs.forEach((item, i) => {
             if (target == item) {
@@ -38,23 +39,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
    // Timer
 
-   const deadLine = '2023-12-25';
+   const deadline = '2023-12-28';
 
    function getTimeRemaining(endtime) {
-      let days, hours, minutes, seconds;
-      const t = Date.parse(endtime) - Date.parse(new Date());
-
-      if (t <= 0) {
-         days = 0;
-         hours = 0;
-         minutes = 0;
-         seconds = 0;
-      } else {
-         (days = Math.floor(t / (1000 * 60 * 60 * 24))),
-            (hours = Math.floor((t / (1000 * 60 * 60)) % 24)),
-            (minutes = Math.floor((t / 1000 / 60) % 60)),
-            (seconds = Math.floor((t / 1000) % 60));
-      }
+      const t = Date.parse(endtime) - Date.parse(new Date()),
+         days = Math.floor(t / (1000 * 60 * 60 * 24)),
+         seconds = Math.floor((t / 1000) % 60),
+         minutes = Math.floor((t / 1000 / 60) % 60),
+         hours = Math.floor((t / (1000 * 60 * 60)) % 24);
 
       return {
          total: t,
@@ -67,7 +59,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
    function getZero(num) {
       if (num >= 0 && num < 10) {
-         return `0${num}`;
+         return '0' + num;
       } else {
          return num;
       }
@@ -97,7 +89,7 @@ window.addEventListener('DOMContentLoaded', () => {
       }
    }
 
-   setClock('.timer', deadLine);
+   setClock('.timer', deadline);
 
    // Modal
 
@@ -105,22 +97,21 @@ window.addEventListener('DOMContentLoaded', () => {
       modal = document.querySelector('.modal'),
       modalCloseBtn = document.querySelector('[data-close]');
 
-   function openModal() {
-      // modal.classList.add('show');
-      // modal.classList.remove('hide');
-      modal.classList.toggle('show');
-      document.body.style.overflow = 'hidden';
-   }
-
    modalTrigger.forEach(btn => {
-      btn.addEventListener('click', openModal());
+      btn.addEventListener('click', openModal);
    });
 
    function closeModal() {
-      // modal.classList.add('hide');
-      // modal.classList.remove('show');
-      modal.classList.toggle('show');
+      modal.classList.add('hide');
+      modal.classList.remove('show');
       document.body.style.overflow = '';
+   }
+
+   function openModal() {
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+      document.body.style.overflow = 'hidden';
+      // clearInterval(modalTimerId);
    }
 
    modalCloseBtn.addEventListener('click', closeModal);
@@ -137,16 +128,87 @@ window.addEventListener('DOMContentLoaded', () => {
       }
    });
 
-   const modalTimerId = setTimeout(openModal, 5000);
+   // const modalTimerId = setTimeout(openModal, 3000);
+   // Закомментировал, чтобы не отвлекало
 
    function showModalByScroll() {
       if (
          window.pageYOffset + document.documentElement.clientHeight >=
-         document.documentElement.scrollHeight - 1
+         document.documentElement.scrollHeight
       ) {
          openModal();
          window.removeEventListener('scroll', showModalByScroll);
       }
    }
    window.addEventListener('scroll', showModalByScroll);
+
+   // Используем классы для создание карточек меню
+
+   class MenuCard {
+      constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+         this.src = src;
+         this.alt = alt;
+         this.title = title;
+         this.descr = descr;
+         this.price = price;
+         this.classes = classes;
+         this.parent = document.querySelector(parentSelector);
+         this.transfer = 27;
+         this.changeToUAH();
+      }
+
+      changeToUAH() {
+         this.price = this.price * this.transfer;
+      }
+
+      render() {
+         const element = document.createElement('div');
+
+         if (this.classes.length === 0) {
+            this.classes = 'menu__item';
+            element.classList.add(this.classes);
+         } else {
+            this.classes.forEach(className => element.classList.add(className));
+         }
+
+         element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
+         this.parent.append(element);
+      }
+   }
+
+   new MenuCard(
+      'assets/images/tabs/vegy.jpg',
+      'vegy',
+      'Меню "Фитнес"',
+      'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+      9,
+      '.menu .container',
+   ).render();
+
+   new MenuCard(
+      'assets/images/tabs/post.jpg',
+      'post',
+      'Меню "Постное"',
+      'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+      14,
+      '.menu .container',
+   ).render();
+
+   new MenuCard(
+      'assets/images/tabs/elite.jpg',
+      'elite',
+      'Меню “Премиум”',
+      'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+      21,
+      '.menu .container',
+   ).render();
 });
